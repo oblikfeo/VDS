6708bb7329a494044448911bfdb1db257b165bb9
Maksim P <email@example.comkfc.kurochka@gmail.com>
2025-01-24 19:50:09 +0600

    final

--- src/app/catalog/[categoryId]/page.ts
+++
@@ -1,8 +0,0 @@
-import { redirect, RedirectType } from 'next/navigation';
-
-type Props = { params: { categoryId: string; page: string } };
-
-export default function Page({ params }: Props) {
-  const { categoryId } = params;
-  redirect(`${categoryId}/1`, RedirectType.replace);
-}
--- src/app/catalog/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect, RedirectType } from 'next/navigation';
-import React from 'react';
-
-export default function Page() {
-  redirect('/', RedirectType.replace);
-}
