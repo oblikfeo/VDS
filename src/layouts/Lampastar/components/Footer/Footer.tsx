672475f1642a07157aaee8e8a7bb9320fb0d63f0
Maksim P <email@example.comkfc.kurochka@gmail.com>
2025-01-28 05:49:23 +0600

    public

--- src/layouts/Lampastar/components/Footer/Footer.tsx
+++ src/layouts/Lampastar/components/Footer/Footer.tsx
@@ -23,7 +23,8 @@ import {
 } from "@layouts/Lampastar/components/Footer/styled";
 import { FOR_CLIENT_MENU } from '../../../../common/constants';
 import { CallMeForm } from '../../../../modules/Contacts';
-import { Logo, Map } from '@resources/images';
+import { Logo } from '@resources/images';
+import Map from '../../../../../public/mapFooter.jpg'
 import { WhatsApp, Telegram, Instagram, Digital } from '../../../../ui/icons';
 
 const { home } = ROUTES;
