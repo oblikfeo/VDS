d4f19402970c99ece757dc6a5d7abf89f2dc2016
Maksim P <email@example.comkfc.kurochka@gmail.com>
2025-01-28 13:57:54 +0600

    unoptimized

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const API_URL = process.env.PROXY_SERVER_URL;
 
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
